590af7159d298e7e2439fa1ac819aed6da3bdf93
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-11 15:30:11 -0400

    progress made on models

---
+++ Seamus_hut_restauraunt_chain/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Seamus_hut_restauraunt_chain.Models
+{
+    public class Address
+    {
+        [Key]
+        public int AddressID { get; set; }
+        public string StreetAddress { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+        public virtual ICollection<Customer> Customers { get; set; }
+
+    }
+}
---
+++ Seamus_hut_restauraunt_chain/Models/Customer..cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Seamus_hut_restauraunt_chain.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int CustomerPhone { get; set; }
+        public string FavoriteDrink { get; set; }
+        public string FavoriteMeal { get; set; }
+        public int LongestBill { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressID { get; set; }
+        public virtual Address Address{ get; set; }
+    }
+}
