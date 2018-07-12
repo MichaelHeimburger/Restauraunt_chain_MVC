1429ae3dbe8b8a66c019f721be584580afa3665a
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-12 10:22:44 -0400

    commiting for saftey

---
+++ Seamus_hut_restauraunt_chain/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Seamus_hut_restauraunt_chain.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public int EmployeePhone { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int EmergencyContactNumber { get; set; }
+        public string EmergencyContactName { get; set; }
+        public DateTime DateStarted { get; set; }
+    }
+}
---
+++ Seamus_hut_restauraunt_chain/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Seamus_hut_restauraunt_chain.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int MenuID { get; set; }
+        public double Price { get; set; }
+        public string ItemName { get; set; }
+    }
+}
